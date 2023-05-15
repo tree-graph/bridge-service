70b20d6e5d62728b1c4d7fa95254b65e077d46c9
Kang <kangjianhu@conflux-chain.org>
2023-05-15 17:31:49 +0800

    switch to go-conflux-utils

--- infra/database/migration.go
+++ infra/database/migration.go
@@ -4,9 +4,7 @@ import (
 	"github.com/tree-graph/bridge-service/models"
 )
 
-//Add list of model add for migrations
-//var migrationModels = []interface{}{&ex_models.Example{}, &model.Example{}, &model.Address{})}
-var migrationModels = []interface{}{&models.Example{}, &models.User{}, &models.CreditCard{},
+var migrationModels = []interface{}{
 	&models.CrossRequest{},
 	&models.Config{},
 	&models.Chain{},
--- infra/logger/logger.go
+++ infra/logger/logger.go
@@ -1,24 +1,10 @@
 package logger
 
 import (
-	"bytes"
 	"github.com/sirupsen/logrus"
-	"strings"
-	"time"
 )
 
-var logger = logrus.New()
-
-func init() {
-	logger.Level = logrus.InfoLevel
-	logger.Formatter = &formatter{}
-
-	logger.SetReportCaller(true)
-}
-
-func SetLogLevel(level logrus.Level) {
-	logger.Level = level
-}
+var logger = logrus.StandardLogger()
 
 type Fields logrus.Fields
 
@@ -60,23 +46,4 @@ func Fatalf(format string, args ...interface{}) {
 		entry := logger.WithFields(logrus.Fields{})
 		entry.Fatalf(format, args...)
 	}
-}
-
-// Formatter implements logrus.Formatter interface.
-type formatter struct {
-	prefix string
-}
-
-// Format building log message.
-func (f *formatter) Format(entry *logrus.Entry) ([]byte, error) {
-	var sb bytes.Buffer
-
-	sb.WriteString(strings.ToUpper(entry.Level.String()))
-	sb.WriteString(" ")
-	sb.WriteString(entry.Time.Format(time.RFC3339))
-	sb.WriteString(" ")
-	sb.WriteString(f.prefix)
-	sb.WriteString(entry.Message)
-
-	return sb.Bytes(), nil
 }
