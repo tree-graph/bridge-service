11d5035720c210cb811c652c95a4ea889d00d9f6
Kang <kangjianhu@conflux-chain.org>
2023-05-16 14:41:24 +0800

    PR

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,9 @@
-/*
-
- */
 package cmd
 
 import (
 	"github.com/Conflux-Chain/go-conflux-util/config"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"gopkg.in/natefinch/lumberjack.v2"
-	"io"
 	"os"
-	"path/filepath"
 )
 
 var cfgFile string
@@ -19,9 +11,6 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use: "bridge",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -40,15 +29,4 @@ func init() {
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	config.MustInit("")
-	operatorSysLogDir := viper.GetString("log.file")
-
-	lumberjackLogger := &lumberjack.Logger{
-		Filename:   filepath.ToSlash(operatorSysLogDir),
-		MaxSize:    5, // MB
-		MaxBackups: 10,
-		MaxAge:     30, // days
-		Compress:   false,
-	}
-	logrus.SetOutput(io.MultiWriter(os.Stdout, lumberjackLogger))
-	logrus.Infof("operatorSysLogDir [%s] \n", operatorSysLogDir)
 }
